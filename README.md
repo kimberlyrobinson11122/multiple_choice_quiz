e3663707ec92e6dcdb0f153b3c98820bba401c1b
Kimberly Robinson <158204628+kimberlyrobinson11122@users.noreply.github.com>
2024-03-26 21:59:42 -0700

    updated high scores list

--- README.md
+++ README.md
@@ -1,1 +1,76 @@
 # multiple_choice_quiz
+
+# My Portfolio
+
+# User Story
+
+AS AN employer
+
+I WANT to view a potential employee's deployed portfolio of work samples
+
+SO THAT I can review samples of their work and assess whether they're a good candidate for an open position
+
+
+## Acceptance Criteria: 
+GIVEN I need to sample a potential employee's previous work:
+
+WHEN I load their portfolio
+    THEN I am presented with the developer's name, a recent photo or avatar, and links to sections about them, their work, and how to contact them
+
+WHEN I click one of the links in the navigation
+    THEN the UI scrolls to the corresponding section
+
+WHEN I click on the link to the section about their work
+    THEN the UI scrolls to a section with titled images of the developer's applications
+
+WHEN I am presented with the developer's first application
+    THEN that application's image should be larger in size than the others
+
+WHEN I click on the images of the applications
+    THEN I am taken to that deployed application
+
+WHEN I resize the page or view the site on various screens and devices
+    THEN I am presented with a responsive layout that adapts to my viewport
+
+## Name
+Kimberly Robinson's Portfolio 
+
+## Deployed Application
+[Visit my deployed portfolio](https://kimberlyrobinson11122.github.io/Kimberly-Robinson-Portfolio/)
+
+## Description
+This deployment is to showcase the projects and work for potential employers and clients
+
+## Solved
+1. We solved the responsive screen issue that was broken in the first build
+
+2. Fixed the project card images to fit and be same sizing
+
+3. Commits do not align as first build was corrupted with conflicting branches, rebuilt commits only show recent work
+
+
+## Images
+Screenshot of the first build:
+![My Portfolio Initial Build](./assets/images/fixed%20links%20and%20spacing.png)
+
+Screenshot of the project cards as they should be, adjusting to sizing from center to one column when shrinking the screen:
+![Project Card Adjustment Success](./assets/images/fixed%20sizing%20issue%20with%20different%20devices.png)
+
+## Credits
+This was built from scratch
+
+
+## Test
+Consistent testing of links throughout each iteration
+
+
+## Authors and acknowledgment
+Appreciation to the Instructor and TAs at EdX Berkeley
+
+
+## Support
+If you need help with anything please email me at kimberly.robinson@outlook.com
+
+
+## Project status
+This is an ongoing project and will be updated as new projects are completed
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,9 @@
-// create questions for quiz
-// create the HTML for the quiz
-// click start the timer starts and the screen shows the first question
-// user answers question
-//   if answer = true, no time is deducted
-//     else answer = false then time decremented by 10s
-// next question
-//     same function repeats
-// view high score
-
-
 var seconds = 200;
 var timer;
 var questionIndex=0; 
 var quizArea=document.querySelector(".quiz_area");
+var grading=document.querySelector("grading")
+
 //var userAnswer;
 var highScore = 0;
 var playerInitials = "";
@@ -21,7 +12,7 @@ var questionsArray = [
   {
     //question 1
     question:
-      "Which functions for adding or removing from an array, are where all three are correct below:",
+      "Which functions for adding or removing from an array, select where all three are correct:",
     choices: [
       "A - pop removes from beginning, unshift adds to the beginning, shift add to the end",
       "B - pop removes from end, shift add to the end, unshift adds to the beginning",
@@ -46,116 +37,116 @@ var questionsArray = [
   },
 
 
-    //question 3
-  { question: 
-      "What is typography in CSS?",
-    choices: 
-    [
-      "the art or procedure of arranging data or processing data on a page",
-      "styling the wireframe",
-      "refers to the CSS properties that allow you to define the color, size, spacing, and shape of text",
-      "adding type of blocks to the structure to the website",
-    ], 
-    answer: "refers to the CSS properties that allow you to define the color, size, spacing, and shape of text"
-  },
+//     //question 3
+//   { question: 
+//       "What is typography in CSS?",
+//     choices: 
+//     [
+//       "the art or procedure of arranging data or processing data on a page",
+//       "styling the wireframe",
+//       "refers to the CSS properties that allow you to define the color, size, spacing, and shape of text",
+//       "adding type of blocks to the structure to the website",
+//     ], 
+//     answer: "refers to the CSS properties that allow you to define the color, size, spacing, and shape of text"
+//   },
 
 
-    //question 4
-  { question: 
-    "What is a viewport?",
-  choices: 
-  [
-    "virtual places within an operating system where network connections start and end",
-    "on a computer it is a place where you can attach another piece of equipment",
-    "represents an area in computer graphics that is currently be viewwed",
-    "is a query of one or more tables that provides another way of presenting the information",
-  ], 
-  answer:"represents an area in computer graphics that is currently be viewwed"
-  },
+//     //question 4
+//   { question: 
+//     "What is a viewport?",
+//   choices: 
+//   [
+//     "virtual places within an operating system where network connections start and end",
+//     "on a computer it is a place where you can attach another piece of equipment",
+//     "represents an area in computer graphics that is currently be viewwed",
+//     "is a query of one or more tables that provides another way of presenting the information",
+//   ], 
+//   answer:"represents an area in computer graphics that is currently be viewwed"
+//   },
 
 
-    //question 5
-  { question: 
-    "Is Responsive Web Design a separate technology?",
-  choices: 
-  [
-    "Yes",
-    "No",
-    "On certain occasions in CSS",
-    "only when used in Javascript",
-  ], 
-  answer:"No"
-  },  
+//     //question 5
+//   { question: 
+//     "Is Responsive Web Design a separate technology?",
+//   choices: 
+//   [
+//     "Yes",
+//     "No",
+//     "On certain occasions in CSS",
+//     "only when used in Javascript",
+//   ], 
+//   answer:"No"
+//   },  
 
 
-   //question 6
-  { question: 
-    "What does CSS stand for?",
-  choices: 
-  [
-    "Cosmic Scripting Scheme",
-    "Creative Style Syntax",
-    "Cascading Style Sheets",
-    "Cybernetic Styling System",
-  ], 
-  answer:"Cascading Style Sheets"
-  },
+//    //question 6
+//   { question: 
+//     "What does CSS stand for?",
+//   choices: 
+//   [
+//     "Cosmic Scripting Scheme",
+//     "Creative Style Syntax",
+//     "Cascading Style Sheets",
+//     "Cybernetic Styling System",
+//   ], 
+//   answer:"Cascading Style Sheets"
+//   },
 
 
-  //question 7
-  { question: 
-  "What directions does Javascript read code?",
-  choices: 
-  [
-  "right to left",
-  "top to bottom",
-  "bottom to top",
-  "left to right",
-  ], 
-  answer:"left to right"
-  },
+//   //question 7
+//   { question: 
+//   "What directions does Javascript read code?",
+//   choices: 
+//   [
+//   "right to left",
+//   "top to bottom",
+//   "bottom to top",
+//   "left to right",
+//   ], 
+//   answer:"top to bottom"
+//   },
 
 
-  //question 8
-  { question: 
-  "Using Javascript, in a function, does RETURN work both inside and outside of the function body",
-  choices: 
-  [
-  "yes, of course",
-  "when RETURN is used correctly, it works both inside and outside",
-  "no, because the teacher said so",
-  "no, RETURN only works inside a function",
-  ], 
-  answer:"no, RETURN only works inside a function"
-  },
+//   //question 8
+//   { question: 
+//   "Using Javascript, in a function, does RETURN work both inside and outside of the function body",
+//   choices: 
+//   [
+//   "yes, of course",
+//   "when RETURN is used correctly, it works both inside and outside",
+//   "no, because the teacher said so",
+//   "no, RETURN only works inside a function",
+//   ], 
+//   answer:"no, RETURN only works inside a function"
+//   },
 
 
-  //question 9
-  { question: 
-  "What is a function in Javascript?",
-  choices: 
-  [
-  "A function is a chunk of code that can be repeated multiple times and is used quite often",
-  "A function in JavaScript is a type of alien life form that communicates with intergalactic servers to summon cosmic energies for website animations",
-  "A function is designed to analyze historical data and predict future trends",
-  "A function in JavaScript is a magical incantation that compels web browsers to dance according to the whims of the developer",
-  ], 
-  answer:"A function is a chunk of code that can be repeated multiple times and is used quite often"
-  },
+//   //question 9
+//   { question: 
+//   "What is a function in Javascript?",
+//   choices: 
+//   [
+//   "A function is a chunk of code that can be repeated multiple times and is used quite often",
+//   "A function in JavaScript is a type of alien life form that communicates with intergalactic servers to summon cosmic energies for website animations",
+//   "A function is designed to analyze historical data and predict future trends",
+//   "A function in JavaScript is a magical incantation that compels web browsers to dance according to the whims of the developer",
+//   ], 
+//   answer:"A function is a chunk of code that can be repeated multiple times and is used quite often"
+//   },
 
 
-//question 10
-  { question: 
-  "What is an array in Javascript?",
-  choices: 
-  [
-  "An array in Javascript is a virtual filing cabinet where the browser stores its favorite memes and GIFs for quick retrieval during moments of digital boredom",
-  "An array in Javascript is a dynamic list of things (objects, functions, anything, etc.)",
-  "An array in Javascript is a mystical portal that transports data to parallel dimensions within the web browser, allowing for multidimensional data manipulation",
-  "An array in Javascript is a sentient entity capable of independent thought, organizing data according to its own mysterious algorithms, and occasionally playing pranks on unsuspecting programmers",
-  ], 
-  answer:"An array in Javascript is a dynamic list of things (objects, functions, anything, etc.)"
-  },
+// //question 10
+//   { question: 
+//   "What is an array in Javascript?",
+//   choices: 
+//   [
+//   "An array in Javascript is a virtual filing cabinet where the browser stores its favorite memes and GIFs for quick retrieval during moments of digital boredom",
+//   "An array in Javascript is a dynamic list of things (objects, functions, anything, etc.)",
+//   "An array in Javascript is a mystical portal that transports data to parallel dimensions within the web browser, allowing for multidimensional data manipulation",
+//   "An array in Javascript is a sentient entity capable of independent thought, organizing data according to its own mysterious algorithms, and occasionally playing pranks on unsuspecting programmers",
+//   ], 
+//   answer:"An array in Javascript is a dynamic list of things (objects, functions, anything, etc.)"
+//   },
   
 {
 
@@ -167,14 +158,11 @@ var questionsArray = [
 function displayNextQuestion() {
     if (questionIndex == questionsArray.length - 1) {
         clearInterval(timer)
-        //var savedScores = localstorage.getItem("Scores") - will have json parsed (wrapping in parat and parse)
-        //if !null then scores = []
-        //clear div, add HTML
-        //input for initials (form-submit event)
-        //event listener to the form
-        //when the user submits it takes the text content from that input (id) 
-        //save to local.storage to object
-        //local.storage.setitem(json stringify({initals: variable, score: variable that represents the current score}))
+        quizArea.classList.add("hidden")
+        document.querySelector(".timer").classList.add("hidden")
+        document.querySelector("#highScoresList").classList.remove("hidden")
+        document.querySelector("#scoreDisplayed").innerHTML = `Good job, you are all finished! <br/> Your final score is ${highScore}.`
+        
         console.log(highScore)
         return
     } else {
@@ -186,7 +174,7 @@ function displayNextQuestion() {
         for (var i = 0; i < choices.length; i++) {
               var button = document.createElement("button")
               button.textContent = choices[i]
-              button.classList.add("choice-button", innerWidth, 25)
+              button.classList.add("choice-button")
               quizArea.appendChild(button)
 
         }
@@ -226,11 +214,17 @@ quizArea.addEventListener("click", function(e){
           // make sure time doesn't go past zero
           if (seconds < 0) {
               seconds = 0
-          }
+         }
       }
       
       // go to the next question
       questionIndex++
+      
+      // timed out grading
+      setTimeout(function() {
+        quizArea.textContent = " "
+      }, 1000)
+
       // display the next question after a small delay
       setTimeout(displayNextQuestion, 1000)
   }
@@ -253,27 +247,61 @@ function start_timer() {
 function userEntersInitials() {
   //add initials
   var inputInitials = document.getElementById("inputInitials").value;
-  if (inputInitials !== "") {
-        winnerInitials = inputInitials.toUpperCase();
-        document.getElementById("initialsDisplayed").textContent = "Initials " + winnerInitials;
-        document.getElementById("initialsContainer").style.display = "none";
-        document.getElementById("highScoreBox").style.display = "block";
-  }
+  console.log(inputInitials)
+  
+      var savedScores = JSON.parse(localStorage.getItem("Scores")) || [] // json parsed
+        
+        savedScores.push({initals: inputInitials, score: highScore})
+        localStorage.setItem("Scores", JSON.stringify(savedScores))
+        document.querySelector(".quiz_area").classList.add("hidden");
+        document.getElementById("leaderBoard").classList.remove("hidden");
+        document.getElementById("highScoresList").classList.remove("hidden");
+        document.querySelector(".timer").classList.add("hidden");
+        updateViewHighScore();
+
+  // if (inputInitials !== "") {
+  //       winnerInitials = inputInitials.toUpperCase();
+  //       document.getElementById("initialsDisplayed").textContent = "Initials " + winnerInitials;
+  //       document.getElementById("initialsContainer").style.display = "none";
+  //       document.getElementById("highScoreBox").style.display = "block";
+  // }
 }
 
 function updateViewHighScore() {
+
     //view high score
-    document.getElementById("scoreDisplayed").textContent = "High Score: " + highScore
+    var savedScores = JSON.parse(localStorage.getItem("Scores")) || [];
+    
+    //high to low
+    savedScores.sort((a, b) => b.score - a.score);
+    console.log(savedScores);
+    //clear
+    document.getElementById("highScoresList").innerHTML = "";
+
+      savedScores.forEach((score, index) => {
+
+        //create list
+        var listItem = document.createElement("li");
+
+        listItem.textContent = `${index + 1}. ${score.initals}: ${score.score}`;
+
+        //Append
+        document.getElementById("players").appendChild(listItem);
+    });
 }
 
 var view_high_scores = document.getElementById("view_high_scores")
+
 view_high_scores.addEventListener("click", function(event) {
+  
     //click will display high score
     event.preventDefault();
-    document.querySelector(".quiz_area").style.display = "none"
-    document.getElementById("highScoreBox").style.display = "block"
-    updateViewHighScore()
-        
+    document.querySelector(".quiz_area").classList.add("hidden");
+    document.getElementById("leaderBoard").classList.remove("hidden");
+    document.getElementById("highScoresList").classList.remove("hidden");
+    document.querySelector(".timer").classList.add("hidden");
+    updateViewHighScore();
+  
 });
 
-document.getElementById("addInitials").addEventListener("click", userEntersInitials)
+document.getElementById("addInitials").addEventListener("click", userEntersInitials);
